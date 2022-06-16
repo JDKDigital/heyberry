8bdd71327705cd3ec34b262866f65aa03fd94258
Jais Djurhuus-Kempel <jdk@combine.dk>
2022-06-17 02:34:35 +0300

    initial version

---
+++ src/main/java/cy/jdkdigital/heyberryshutup/HeyBerry.java
@@ -0,0 +1,35 @@
+package cy.jdkdigital.heyberryshutup;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+import java.util.stream.Collectors;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(HeyBerry.MODID)
+public class HeyBerry
+{
+    // Directly reference a slf4j logger
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public static final String MODID = "heyberryshutup";
+
+    public HeyBerry()
+    {
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.CONFIG);
+    }
+}
---
+++ src/main/java/cy/jdkdigital/heyberryshutup/mixin/MixinSweetBerryBushBlock.java
@@ -0,0 +1,33 @@
+package cy.jdkdigital.heyberryshutup.mixin;
+
+import cy.jdkdigital.heyberryshutup.Config;
+import cy.jdkdigital.heyberryshutup.HeyBerry;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.npc.AbstractVillager;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.SweetBerryBushBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.Vec3;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = SweetBerryBushBlock.class)
+public class MixinSweetBerryBushBlock {
+
+    @Inject(
+            at = {@At("HEAD")},
+            method = {"entityInside(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/entity/Entity;)V"},
+            cancellable = true
+    )
+    public void entityInside(BlockState blockState, Level level, BlockPos pos, Entity entity, CallbackInfo callbackInfo) {
+        if ((Config.COMMON.disableVillagerDamage.get() && entity instanceof AbstractVillager) || (Config.COMMON.disableDamageWhenArmored.get() && entity instanceof LivingEntity && !((LivingEntity)entity).getItemBySlot(EquipmentSlot.LEGS).isEmpty() && !((LivingEntity)entity).getItemBySlot(EquipmentSlot.FEET).isEmpty())) {
+            entity.makeStuckInBlock(blockState, new Vec3(0.8F, 0.75D, 0.8F));
+            callbackInfo.cancel();
+        }
+    }
+}
